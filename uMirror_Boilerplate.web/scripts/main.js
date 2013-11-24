a487edbeff7d0f403bb37ed25108e10d0d94674a
Nathalie Larsson <natha.larsson@gmail.com>
2013-11-24 17:11:32 +0100

    Page helper and more

--- uMirror_Boilerplate.web/scripts/main.js
+++ uMirror_Boilerplate.web/scripts/main.js
@@ -2,8 +2,8 @@
 
 	$('.simpel-slideshow').unslider({
 		speed: 500,               //  The speed to animate each slide (in milliseconds)
-		delay: 3000,              //  The delay between slide animations (in milliseconds)
-		complete: function () { console.log("Yeay") },  //  A function that gets called after every slide animation
+		delay: 6000,              //  The delay between slide animations (in milliseconds)
+		complete: function () { },  //  A function that gets called after every slide animation
 		keys: true,               //  Enable keyboard (left, right) arrow shortcuts
 		dots: false,               //  Display dot navigation
 		fluid: false              //  Support responsive design. May break non-responsive designs
